17c984bbaea60ca8420222b67a7057116dfb3dd6
Ryan Turner <RyanTurner02@protonmail.com>
2023-09-19 15:43:18 -0400

    Set up battle scene layout

---
+++ Volcanic Bird/battleScene/Enemy.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var level
+var health_points
+var magic_points
+var defense
+var speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Volcanic Bird/battleScene/Player.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var level
+var experience
+var health_points
+var magic_points
+var defense
+var speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
