be8b948ab7eceb7dd25976b7feb29e7249da61e3
Ryan Turner <RyanTurner02@protonmail.com>
2023-09-20 17:29:24 -0400

    Change enemy health in battle scene

--- Volcanic Bird/battleScene/Enemy.gd
+++ Volcanic Bird/battleScene/Enemy.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var asset : Resource
+@export var current_hp : int
+@export var max_hp : int
 
 var level
 var health_points
@@ -10,9 +12,14 @@ var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Load the texture of the enemy
 	if asset != null:
 		$"Enemy Container/Sprite".texture = asset
-
+	
+	# Update health
+	$"Enemy Container/Health Bar".value = current_hp
+	$"Enemy Container/Health Bar".max_value = max_hp
+	$"Enemy Container/Health Bar/Label".text = str(current_hp) + "/" + str(max_hp)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
