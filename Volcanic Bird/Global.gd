a066a22edcb1cb20cf83c8d90c8b8dc8535ded05
Nikola Bjelica <100382766+NikolaBjelica@users.noreply.github.com>
2023-10-03 15:50:54 -0400

    I may have fixed it

--- Volcanic Bird/Global.gd
+++ Volcanic Bird/Global.gd
@@ -1,13 +1,14 @@
 extends Node
 
 signal battleGroup_changed
-signal updateItems
+
+# @onready var playerBattleGroup_buttongroup = ButtonGroup.new()
 
 # Player Items and Other needed details
-var battleGroup = ["res://Creatures/Tree.tres", null, null, null] # 4x null because of 4 creatures in group
+var battleGroup = [null, null, null, null] # 4x null because of 4 creatures in group
 var creatureStorage = ["res://Creatures/Tree.tres", "res://Creatures/Purple_Flower.tres"]
-var itemInventory = ["res://Creatures/Tree.tres"] 
-var itemStorage = ["res://Creatures/Purple_Flower.tres"]
+var itemInventory = [] 
+var itemStorage = []
 
 # Creature Parts For Creature Menu
 var current_name
@@ -17,12 +18,6 @@ var current_description
 var draggedCreature
 var draggedCreatureIndex = 0
 
-var draggedItemInventory
-var draggedItemIncentoryIndex = 0
-
-var draggedItemStorage
-var draggedItemStorageIndex = 0
-
 
 func addToBattleGroup():
 	print(draggedCreatureIndex)
@@ -31,22 +26,6 @@ func addToBattleGroup():
 			battleGroup[battleGroup.find(draggedCreature)] = null
 	battleGroup[draggedCreatureIndex] = draggedCreature
 	emit_signal("battleGroup_changed")
-	
-func addToInventory():
-	print("1")
-	if draggedItemStorage != null:
-		print("2")
-		itemInventory.append(draggedItemStorage)
-		itemStorage.erase(draggedItemStorage)
-		draggedItemStorage = null
-	emit_signal("updateItems")
-
-func addToStorage():
-	if draggedItemInventory != null:
-		itemStorage.append(draggedItemInventory)
-		itemInventory.erase(draggedItemInventory)
-		draggedItemInventory = null
-	emit_signal("updateItems")
 	
 func removeFromBattleGroup():
 	pass
