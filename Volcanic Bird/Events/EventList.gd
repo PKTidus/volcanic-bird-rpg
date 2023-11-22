f7290a21d2d8f7ef5b976023b2bcc9fe59f4242a
twallie <thomaswallsmith@gmail.com>
2023-11-22 18:05:36 -0500

    feat: added Tree event

--- Volcanic Bird/Events/EventList.gd
+++ Volcanic Bird/Events/EventList.gd
@@ -73,4 +73,80 @@ static var List = [
 			]
 		), 
 	),
+	Event.new(
+		"Slumbering Trees", 
+		
+		"You stumble upon a group of three trees. You notice these are no normal trees, but living trees.", 
+		
+		# option A
+		Option.new(
+			"Gently wake them.",
+			[
+				OptionResult.new(
+					"They welcome your presence and wish to join you. 3 Trees can be found in creature storage.", # description
+					"escape", # type
+					70, # chance
+					[]
+				),
+				OptionResult.new(
+					"Two of the trees are upset with being awaken, but one wishes to join you. 1 Tree can be found in creature storage.",
+					"escape",
+					30,
+					[]
+				)
+			]
+		), 
+		
+		# option B
+		Option.new(
+			"Chop their wood.",
+			[
+				OptionResult.new(
+					"The trees are chopped before they have a chance to fight back. They wimper quietly as their sap is drained by your blades.",
+					"escape",
+					75,
+					[]
+				),
+				OptionResult.new(
+					"Before you can swing, a tree you didn't notice steps between you and knocks you back.", # description
+					"battle", # type
+					25, # chance
+					[ # enemies
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres"
+					]
+				)
+			]
+		), 
+		
+		# option C
+		Option.new(
+			"Set fire",
+			[
+				OptionResult.new(
+					"You are able to torch one of the trees before they are able to attack.", # description
+					"battle", # type
+					60, # chance
+					[ # enemies
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres"
+					]
+				),
+				OptionResult.new(
+					"You are unable to torch any of the trees before they are able to attack.", # description
+					"battle", # type
+					40, # chance
+					[ # enemies
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres",
+						"res://Enemies/TreeEnemy.tres"
+					]
+				)
+			]
+		), 
+	),
 ]
