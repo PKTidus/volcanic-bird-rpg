3ad59981761b4157be5e8ef0cadff2d97a2d8da9
Nikola Bjelica <100382766+NikolaBjelica@users.noreply.github.com>
2023-11-28 14:15:54 -0500

    Added goated game over scene

--- Volcanic Bird/GameOver.gd
+++ Volcanic Bird/GameOver.gd
@@ -5,15 +5,8 @@ extends Control
 func _ready():
 	$Button.hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Main Menu/main_menu.tscn")
-
 
 func _on_video_stream_player_finished():
 	$VideoStreamPlayer.play()
