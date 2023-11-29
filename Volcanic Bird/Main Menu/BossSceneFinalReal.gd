7f517985dbf67e8aa219a346d58c462783ee5989
PKTidus <pedrotk@fastmail.com>
2023-11-29 10:51:58 -0500

    Final story edits and manual

--- Volcanic Bird/Main Menu/BossSceneFinalReal.gd
+++ Volcanic Bird/Main Menu/BossSceneFinalReal.gd
@@ -11,7 +11,7 @@ func _on_option_b_pressed():
 	myArray[1] = "res://Enemies/VolcanicBirdEnemy.tres"
 	myArray[2] = "res://Enemies/VolcanicCloneEnemy.tres"
 	Global.selected_enemy_paths = myArray
-	$EventDescription.text = "This is good, very based, well done. Pls save the world"
+	$EventDescription.text = "You fight for a noble cause! Will you be humbled by the root of all evil?"
 	Global.background = "res://Resources/Backgrounds/ice.png"
 	Global.whichEnding = 2
 	
@@ -25,7 +25,7 @@ func _on_option_a_pressed():
 	myArray[1] = "res://Enemies/UltraCrowillaEnemy.tres"
 	myArray[2] = "res://Enemies/CrowillaEnemy.tres"
 	Global.selected_enemy_paths = myArray
-	$EventDescription.text = "You decided to join the very thing that is seeking to end the world! But no fear, the Crowilla clan and it's clan leader, CHAD the Crowilla will try to put an end to your madness! How dare you!!!!!"
+	$EventDescription.text = "You decided to join the very thing that is seeking to end the world! A powerful energy approaches... the leader of the crowillas has completed his training and is here to stop you and the Volcanic Bird!"
 	Global.background = "res://Resources/Backgrounds/forest.png"
 	Global.whichEnding = 1 
 	
--- Volcanic Bird/Main Menu/manual.gd
+++ Volcanic Bird/Main Menu/manual.gd
@@ -41,37 +41,63 @@ func showInPageButtons():
 func loadPageOne():
 	page = 1
 	showInPageButtons()
-	$Description.text = "Page 1"
+	$Description.text = "So you want to be an adventurer? Get ready to learn all there is to know with yours truly, Lucian Venture, the greatest adventurer to ever live! 
+	\nThe basics of adventuring are as follows: 
+		when you hit play you'll see a new event! 
+		You get three choices! 
+		They rarely have a single possible outcome!\n
+		Try everything and find the most efficient way to reach the boss!\n
+		When reaching the threshold for events, you can move onto the next floor with a new threshold."
 	$PageNum.text = "1"
 
 func loadPageTwo():
 	page = 2
 	showInPageButtons()
-	$Description.text = "Page 2"
+	$Description.text = "A great adventurer knows how to fight! We don't do any fighting ourselves however... that's what our trusty creatures are for!\n
+	In battles you get five options each turn:\n
+	Attack: this is your most basic method of dealing damage and uses your creature's attack stat to calculate how much you do.\n
+	Skill: this is your most vital asset to dealing massive damage or gaining a tactical advantage! Magical characters prefer to hit with spells, which strike at the enemies' magic defense! Buffs, debuffs and healing are great too!\n
+	Defend: this will lower the damage your creature takes on that turn!
+	Item: this will allow you to use your various items you've accumulated!\n
+	Run: if you don't think you can win, you can always run away!"
 	$PageNum.text = "2"
 
 func loadPageThree():
 	page = 3
 	showInPageButtons()
-	$Description.text = "Page 3"
+	$Description.text = "Ah, your majestic creatures! Use them well to aid you on your quest!
+	Their attack damage and magic attack damage determine how much damage they can deal with physical attacks/skills and magical skills respectively. Check skill descriptions for what stats are used in calculations!\n
+	Your creatures' defense stats are used to divide the damage taken from attacks they are receiving, remember: a strong defense is the best offense... or something like that\n
+	Your creatures' HP is how much damage they can take before dying... fear not! When a creature dies you can still get them back up with a revival item or leveling up!\n
+	Similar things are true for MP, though this is only used to cast skills, you may also exceed the maximum by using items.\n
+	Speed will determine turn order, get the jump on your enemies with blazing speed!"
 	$PageNum.text = "3"
 
 func loadPageFour():
 	page = 4
 	showInPageButtons()
-	$Description.text = "Page 4"
+	$Description.text = "Enemies are scary and get stronger with the more events you witness!\n
+	They have a chance to attack your lowest health creature in attempts to swipe a kill and also have the capability to target your weaker defensive stat!\n
+	Outrageous!\n"
 	$PageNum.text = "4"
 
 func loadPageFive():
 	page = 5
 	showInPageButtons()
-	$Description.text = "Page 5"
+	$Description.text = "Every adventurer's true calling is to reach the 10 event threshold and move on to floor 2 but this is only the beginning!\n
+	After reaching floor 2 you'll see the 15 event threshold... once this is reached the next floor button will take you to the climactic finale of your adventure!\n
+	If you can defeat a final boss, you win! If you couldn't make it, give it another shot. Try new creatures, strategies and event choices."
 	$PageNum.text = "5"
 
 func loadPageSix():
 	page = 6
 	showInPageButtons()
-	$Description.text = "Page 6"
+	$Description.text = "Some tips from yours truly:\n
+	Look for events that level up your party and fight when you can win, the rewards are worth it.\n
+	Events that give your party stat boosts are also good.\n
+	It may be worthwhile recruiting creatures later in your journey, their base stats will be improved even though their level is 1!\n
+	While later battles are more difficult, the experience bounty will be much greater!\n
+	Enemies always drop items, make good use of them!\n"
 	$PageNum.text = "6"
 
 func _on_go_left_pressed():
